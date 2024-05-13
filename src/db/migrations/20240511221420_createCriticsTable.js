a53bea3de441cc8850b4e33516173020f5c65f10
Adan <jonesadan@yahoo.com>
2024-05-12 21:39:07 -0400

    created the rest of the tables

--- src/db/migrations/20240511221420_createCriticsTable.js
+++ src/db/migrations/20240511221420_createCriticsTable.js
@@ -1,8 +1,13 @@
-
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  return knex.schema.createTable("critics", (table) => {
+    table.increments("critic_id").primary();
+    table.string("preferred_name");
+    table.string("surname");
+    table.string("organization_name");
+    table.timestamps(true, true);
+  });
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema.dropTable("critics");
 };
