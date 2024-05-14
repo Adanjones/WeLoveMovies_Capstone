825e1f7ad3db6b6946c3cb203ec043a6b71c33d7
Adan <jonesadan@yahoo.com>
2024-05-13 22:06:35 -0400

    updated express route endpoints

--- src/app.js
+++ src/app.js
@@ -1,5 +1,29 @@
 if (process.env.USER) require("dotenv").config();
+
+const morgan = require("morgan");
 const express = require("express");
+const cors = require("cors");
+
+const moviesRouter = require("./movies_route/movies.router");
+const reviewsRouter = require("./reviews_route/reviews.router");
+const theatersRouter = require("./theaters_route/theaters.router");
+
+const errorHandler = require("./errors/errorHandler");
+const notFound = require("./errors/notFound");
+
 const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+app.use(morgan("dev"));
+
+app.use("/movies", moviesRouter);
+app.use("/reviews", reviewsRouter);
+app.use("/theaters", theatersRouter);
+
+app.use(notFound);
+app.use(errorHandler);
 
 module.exports = app;
