7b3c7fdf2f7048a9487012e4eafef60ba94dafc1
Adan <jonesadan@yahoo.com>
2024-05-24 23:23:48 -0400

    added port variable

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ if (process.env.USER) require("dotenv").config();
 const morgan = require("morgan");
 const express = require("express");
 const cors = require("cors");
+const app = express();
 
 const moviesRouter = require("./movies_route/movies.router");
 const reviewsRouter = require("./reviews_route/reviews.router");
@@ -11,8 +12,7 @@ const theatersRouter = require("./theaters_route/theaters.router");
 const errorHandler = require("./errors/errorHandler");
 const notFound = require("./errors/notFound");
 
-const app = express();
-const port = process.env.PORT || 4000; // Use PORT environment variable
+const { PORT = 5001 } = process.env; // Use PORT environment variable
 
 app.use(express.json());
 
@@ -29,8 +29,8 @@ app.use("/theaters", theatersRouter);
 app.use(notFound);
 app.use(errorHandler);
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
 module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,14 +1,12 @@
-const { PORT = 5001 } = process.env;
-
 const app = require("./app");
 const knex = require("./db/connection");
 
-const listener = () => console.log(`Listening on Port ${PORT}!`);
+const listener = () => console.log(`Listening on Port ${process.env.PORT}!`);
 
 knex.migrate
   .latest()
   .then((migrations) => {
     console.log("migrations", migrations);
-    app.listen(PORT, listener);
+    listener();
   })
   .catch(console.error);
